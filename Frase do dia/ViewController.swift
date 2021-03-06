7ea886e0285cce7ed2b7f81d1016e6efbf6487df
Caio <caiovr13@gmail.com>
2021-03-06 17:38:07 -0300

    Layout do app

--- Frase do dia/ViewController.swift
+++ Frase do dia/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func newPhraseButton(_ sender: UIButton) {
+        phraseLabel.text = getNewPhrase()
+        
+    }
+    @IBOutlet weak var phraseLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func getNewPhrase() -> String {
+        return ""
     }
 
 
